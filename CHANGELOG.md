e724ae8bc756d73481ef3e6282a9b3582ac480fe
Dominik Krajcer <dom.kraj@gmail.com>
2023-03-15 22:48:01 +0100

    0.4.0
    * update dependencies
    
    * add web support
    
    * add web screenshot
    
    * update README
    
    * update CHANGELOG
    
    * bump version to 0.4.0
    
    * update dependencies
    
    * adjust source to main
    
    * format code
    
    * adjust build.yaml

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.4.0
+
+Added web support
+
 ## 0.3.0
 
 Added windows support
---
+++ lib/system_date_time_format_web.dart
@@ -0,0 +1,53 @@
+// In order to *not* need this ignore, consider extracting the "web" version
+// of your plugin as a separate package, instead of inlining it in the same
+// package as the core of your plugin.
+// ignore: avoid_web_libraries_in_flutter
+
+@JS()
+library system_date_time_format;
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:js/js.dart';
+import 'package:system_date_time_format/src/system_date_time_format_platform_interface.dart';
+
+@JS('getDateFormat')
+external String _getDateFormat();
+
+@JS('getLongDateFormat')
+external String _getLongDateFormat();
+
+@JS('getMediumDateFormat')
+external String _getMediumDateFormat();
+
+@JS('getTimeFormat')
+external String _getTimeFormat();
+
+/// A web implementation of the SystemDateTimeFormatPlatformInterface of the SystemDateTimeFormat plugin.
+class SystemDateTimeFormatWeb extends SystemDateTimeFormatPlatformInterface {
+  /// Constructs a SystemDateTimeFormatWeb
+  SystemDateTimeFormatWeb();
+
+  static void registerWith(Registrar registrar) {
+    SystemDateTimeFormatPlatformInterface.instance = SystemDateTimeFormatWeb();
+  }
+
+  @override
+  Future<String> getDateFormat({required String fallback}) async {
+    return _getDateFormat();
+  }
+
+  @override
+  Future<String> getLongDateFormat({required String fallback}) async {
+    return _getLongDateFormat();
+  }
+
+  @override
+  Future<String> getMediumDateFormat({required String fallback}) async {
+    return _getMediumDateFormat();
+  }
+
+  @override
+  Future<String> getTimeFormat({required String fallback}) async {
+    return _getTimeFormat();
+  }
+}
